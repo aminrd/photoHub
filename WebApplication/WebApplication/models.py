8663d5d32eb960577597b64016ad51e7b46123a0
aminrd <aghaee@cs.ubc.ca>
2020-03-09 17:46:22 -0700

    uploading views.py

--- WebApplication/WebApplication/models.py
+++ WebApplication/WebApplication/models.py
@@ -11,7 +11,7 @@ from io import BytesIO
 from PIL import Image
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import os
-
+import statistics
 
 # ==================================================
 # Define User, Client and Designer tables
@@ -52,10 +52,30 @@ class Client(UserInfo):
     # List of projects: client_project <list>
     pass
 
+
 class Designer(UserInfo):
     score = models.IntegerField(default=0)
-    avg_feedback = models.DecimalField(max_digits=3, decimal_places=1 ,default=0.0)
+    avg_feedback = models.DecimalField(max_digits=3, decimal_places=1, default=0.0)
     rate = models.IntegerField(default=1)
+
+    # Public profile visible to all people
+    user_plus = models.IntegerField(default=False)
+    user_plus_expires = models.DateTimeField(auto_now_add=True)
+
+    # User links for user_plus only:
+    homepage_link = models.CharField(max_length=1024, blank=True, null=True, default=None)
+    instagram_url = models.CharField(max_length=1024, blank=True, null=True, default=None)
+    linkedin_url = models.CharField(max_length=1024, blank=True, null=True, default=None)
+
+    def update_avg_feedback(self):
+        plist = list(self.project_server.all())
+        if len(plist) < 1:
+            self.avg_feedback = 0.0
+        else:
+            self.avg_feedback = sum(plist) / len(plist)
+        self.save()
+
+
 
 # ==================================================
 # Define User, Client and Designer tables
@@ -75,7 +95,7 @@ class Project(models.Model):
 
     # Users related
     client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, related_name='project_client')
-    server = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True, related_name='project_server', default=None)
+    server = models.ForeignKey(Designer, on_delete=models.SET_NULL, null=True, related_name='project_server', default=None)
     applicants = models.ManyToManyField(Designer)
 
     allowed_to_share = models.BooleanField(default=False, blank=True, null=True)
@@ -93,12 +113,19 @@ class Project(models.Model):
     output_image = models.ImageField(upload_to='images/outputs/main/', default=None)
     output_image_thumbnail = models.ImageField(upload_to='images/outputs/thumbnail/', default=None)
 
+    def vote_feedback(self, value):
+        self.owner_feedback = value
+        if self.owner_feedback is not None and self.owner_feedback != value:
+            value = value - self.owner_feedback
+        self.save()
+        self.server.update_avg_feedback()
+
     def create_thumbnails(self, related_list):
         # Relate list example: [(self.input_image, self.input_image_thumbnail)]
         for main_image, thum_image in related_list:
             image = Image.open(main_image.file)
 
-            w,h = image.size
+            w, h = image.size
             ratio = w / 800
             image.thumbnail(size=(int(w / ratio), int(h / ratio)))
 
@@ -123,7 +150,9 @@ class Project(models.Model):
         if os.path.isfile(f_path):
             self.input_file = None
             os.remove(f_path)
+        self.save()
         return True
+
 
 class AdvancedProject(Project):
     input_file1 = models.FileField(upload_to='images/inputs/original_file', default=None)
@@ -143,14 +172,21 @@ class AdvancedProject(Project):
     input_image4_thumbnail = models.ImageField(upload_to='images/inputs/thumbnail/', default=None)
 
     def clear_disk(self):
+        f_path = os.path.join(app_setting.MEDIA_ROOT, self.input_file)
         f_path1 = os.path.join(app_setting.MEDIA_ROOT, self.input_file1)
         f_path2 = os.path.join(app_setting.MEDIA_ROOT, self.input_file2)
         f_path3 = os.path.join(app_setting.MEDIA_ROOT, self.input_file3)
         f_path4 = os.path.join(app_setting.MEDIA_ROOT, self.input_file4)
 
-        for f_path in (f_path1, f_path2, f_path3, f_path4):
+        self.input_file = None
+        self.input_file1 = None
+        self.input_file2 = None
+        self.input_file3 = None
+        self.input_file4 = None
+
+        for f_path in (f_path, f_path1, f_path2, f_path3, f_path4):
             if os.path.isfile(f_path):
-                self.input_file = None
                 os.remove(f_path)
 
+        self.save()
         return True
