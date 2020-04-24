04be1f69c04f830fa961297e63ce6dfdfe57b857
aminrd <aghaee@cs.ubc.ca>
2020-04-23 19:14:47 -0700

    Password reset procedure update

--- WebApplication/WebApplication/settings.py
+++ WebApplication/WebApplication/settings.py
@@ -139,4 +139,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # EMAIL_HOST_USER = 'test@gmail.com'
 # EMAIL_HOST_PASSWORD = 'test123##'
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = '127.0.0.1'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
--- WebApplication/WebApplication/urls.py
+++ WebApplication/WebApplication/urls.py
@@ -50,11 +50,11 @@ urlpatterns = [
     path('balance/', views.balance, name='balance'),
 
     # Password reset management:
-    re_path(r'^password_reset/$', auth_views.PasswordResetView, name='password_reset'),
-    re_path(r'^password_reset/done/$', auth_views.PasswordResetDoneView, name='password_reset_done'),
+    re_path(r'^password_reset/$', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    re_path(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView, name='password_reset_confirm'),
-    re_path(r'^reset/done/$', auth_views.PasswordResetCompleteView, name='password_reset_complete'),
+        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    re_path(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
