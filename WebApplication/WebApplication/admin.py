37346f8d1b5c39e29e3ed56e851e7a5f9305e658
Amin <amin.rad.1371@gmail.com>
2024-03-21 19:06:17 -0700

    preparing railsways ready branch

--- WebApplication/WebApplication/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Client)
-admin.site.register(Designer)
-admin.site.register(Project)
-admin.site.register(AdvancedProject)
-admin.site.register(Notification)
--- WebApplication/WebApplication/apps.py
+++
@@ -1,6 +0,0 @@
-# Licence: photoHub
-# Written by Amin Aghaee
-from django.apps import AppConfig
-
-class WebApplicationConfig(AppConfig):
-    name = 'WebApplication'
