3cfd8e82aea29186fa12f96f1da557ca3fd5e5a9
Amin Aghaee <amin.rad.1371@gmail.com>
2024-03-21 19:46:34 -0700

    whitenoise handling static files

--- WebApplication/settings.py
+++ WebApplication/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
 
     # Third-party packages:
     'phonenumber_field',
+
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -46,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'WebApplication.urls'
@@ -128,6 +131,8 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 
